b7093fb1faa4d0b5f1ed09d1bc8f298a1c4d0ba7
Tom Tarpey <tomtarpeydev@gmail.com>
2025-11-06 20:24:10 +0000

    Add create_remaining.sh script to track files for upcoming DsecOS Enterprise briefings, listing required index.html files for various topics.

---
+++ docs/briefings/breifings-web/create_remaining.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script will help track which files need to be created
+echo "Files to create:"
+echo "1. quantum/index.html"
+echo "2. rwa-platform/index.html"
+echo "3. sat-ground-station/index.html"
+echo "4. sovereign-cloud/index.html"
+echo "5. supply-chain/index.html"
+echo "6. synthetic-insulin/index.html"
